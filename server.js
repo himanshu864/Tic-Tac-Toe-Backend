f6b009dc467573063c66a3f22645d984c25f705b
himanshu864 <himanshu864000@gmail.com>
2025-03-19 14:18:49 +0530

    added create and join room functionality

--- server.js
+++ server.js
@@ -4,15 +4,39 @@ const io = require("socket.io")(3000, {
   },
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected!", socket.id);
+const { v4: uuidv4 } = require("uuid");
 
-  // Listen and Emit custom events
-  socket.on("foo", (msg) => {
-    io.emit("bar", `${msg} from ${socket.id}`);
+const rooms = {};
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+
+  socket.on("create-room", (cb) => {
+    try {
+      const room = uuidv4();
+      rooms[room] = [socket.id];
+      cb(null, room);
+    } catch (error) {
+      cb({ error: "Room creation failed" });
+    }
+  });
+
+  socket.on("join-room", (room, cb) => {
+    try {
+      if (!room || !rooms[room] || rooms[room].length === 2) {
+        return cb({ error: "Invalid room or room is full" });
+      }
+      rooms[room].push(socket.id);
+      const playerSocket = rooms[room][0];
+      socket.to(playerSocket).emit("opponent-join");
+      cb(null);
+    } catch (error) {
+      cb({ error: "Failed to join room" });
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected!", socket.id);
+    console.log(`Socket disconnected: ${socket.id}`);
+    // Optionally, you could also remove the socket from any room here.
   });
 });
